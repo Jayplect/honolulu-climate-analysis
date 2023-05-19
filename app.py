626c85efaf5ca240af8a84d9f449d9b6e3182e40
Jayplect <evansja.femi@gmail.com>
2023-05-19 03:18:08 -0400

    added data files

---
+++ app.py
@@ -0,0 +1,30 @@
+# Import the dependencies.
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
