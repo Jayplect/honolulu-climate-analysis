0bd288e491102a8fbeba46c94f7a10353266fa3a
Jayplect <evansja.femi@gmail.com>
2023-05-19 21:34:19 -0400

    added both database and flask setup

--- SurfsUp/app.py
+++ SurfsUp/app.py
@@ -10,24 +10,25 @@ from flask import Flask,jsonify
 #################################################
 # Database Setup
 #################################################
-
+database_path = "../Resources/hawaii.sqlite"
+engine = create_engine(f"sqlite:///{database_path}")
 
 # reflect an existing database into a new model
-
+Base = automap_base()
 # reflect the tables
-
+Base.prepare(autoload_with=engine)
 
 # Save references to each table
-
+Measurement = Base.classes.measurement
+Station = Base.classes.station
 
 # Create our session (link) from Python to the DB
-
+session = Session(engine)
 
 #################################################
 # Flask Setup
 #################################################
-
-
+app = Flask(__name__)
 
 
 #################################################
